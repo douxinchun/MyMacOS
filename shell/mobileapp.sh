20820b0678acfff1858f98a6b04c0a7b80d61c01
NewSpring <douxinchunlengyu@163.com>
2017-12-02 14:17:13 +0800

    rename file name;add double quotes for

--- shell/mobileapp.sh
+++ shell/mobileapp.sh
@@ -5,6 +5,6 @@ then
   exit 1
 fi
 # mobileprovision_uuid=`grep UUID -A1 -a $1 | grep -o "[-A-Z0-9]\{36\}"`
-mobileprovision_uuid=`/usr/libexec/PlistBuddy -c "Print UUID" /dev/stdin <<< $(/usr/bin/security cms -D -i $1)`
+mobileprovision_uuid=`/usr/libexec/PlistBuddy -c "Print UUID" /dev/stdin <<< $(/usr/bin/security cms -D -i "$1")`
 echo "UUID is:"
 echo ${mobileprovision_uuid}
