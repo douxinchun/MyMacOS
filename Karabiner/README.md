fc4dc97efb18d649049ae128b84d23d8ce4e6d3f
xinchundou <douxinchun@gmail.com>
2023-09-20 13:56:44 +0800

    Add karabiner configuration file

---
+++ Karabiner/README.md
@@ -0,0 +1,6 @@
+Karabiner-Elements 14.2.0  
+
+[The location of the configuration file](https://karabiner-elements.pqrs.org/docs/manual/misc/configuration-file-path/)
+
+karabiner 配置文件位置：Karabiner/karabiner  
+`LoadConfigurationFiles`会通过软连接的形式link改配置文件夹到了 ~/.config/karabiner
--- Scripts/LoadConfigurationFiles.sh
+++ Scripts/LoadConfigurationFiles.sh
@@ -1,8 +1,18 @@
 #!/bin/zsh
 
 # The script loads the configuration files from the repository to the user's home directory.
-
+CURRENT_PATH=$(pwd)
+SCRIPT_PATH=$(dirname "$0")
+echo "Move to ${SCRIPT_PATH}"
+cd $SCRIPT_PATH
 REPOSITORY_PATH=$(git rev-parse --show-toplevel) 
 OH_MY_ZSH_LOCATION_IN_PROJECT="${REPOSITORY_PATH}/shell/oh-my-zsh"
 
-cp -f $OH_MY_ZSH_LOCATION_IN_PROJECT ~/.zshrc
+cp -f $OH_MY_ZSH_LOCATION_IN_PROJECT/.zshrc ~/.zshrc
+
+# Karabiner-Elements
+ln -snf "${REPOSITORY_PATH}/Karabiner/karabiner" ~/.config/karabiner
+launchctl kickstart -k gui/`id -u`/org.pqrs.karabiner.karabiner_console_user_server
+
+echo "Back to ${CURRENT_PATH}"
+cd $CURRENT_PATH
